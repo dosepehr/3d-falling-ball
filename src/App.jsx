1a163093ede3fd4ed8637135d60f12ee356fd973
dosepehr <dosepehr@gmail.com>
2023-12-09 16:22:02 +0330

    add lights & shadows

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,37 @@
 import { OrbitControls } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
-import React from 'react';
+import { useFrame } from '@react-three/fiber';
+import React, { useRef } from 'react';
 
 const App = () => {
+    const cube = useRef();
+    const group = useRef();
+    useFrame((state, delta) => {
+        const time = state.clock.elapsedTime;
+
+        const x = Math.cos(time) * 2;
+        const z = Math.sin(time) * 2;
+        group.current.position.x = x;
+        group.current.position.z = z;
+    });
     return (
         <>
-            <Canvas
-                camera={{
-                    fov: 45,
-                    near: 0.1,
-                    far: 200,
-                    position: [3, 2, 6],
-                }}
-            >
-                <OrbitControls />
-                <mesh position={[3, 0, 0]}>
+            <directionalLight position={[1, 2, 3]} intensity={1.5} castShadow />
+            <ambientLight intensity={1.5} />
+            <OrbitControls />
+            <group ref={group}>
+                <mesh position={[3, 0, 0]} castShadow>
                     <sphereGeometry args={[1]} />
-                    <meshBasicMaterial color='yellow' />
+                    <meshStandardMaterial color='yellow' />
                 </mesh>
-                <mesh position={[0, -1, 0]} rotation-x={-Math.PI / 2}>
-                    <planeGeometry args={[15, 15]} />
-                    <meshBasicMaterial color='green' />
-                </mesh>
-                <mesh position={[-3, 0, 0]}>
+                <mesh position={[-3, 0, 0]} castShadow ref={cube}>
                     <boxGeometry args={[2, 2, 2]} />
-                    <meshBasicMaterial color='skyblue' />
+                    <meshStandardMaterial color='skyblue' />
                 </mesh>
-            </Canvas>
+            </group>
+            <mesh position={[0, -1, 0]} rotation-x={-Math.PI / 2} receiveShadow>
+                <planeGeometry args={[15, 15]} />
+                <meshStandardMaterial color='green' />
+            </mesh>
         </>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import { Canvas } from '@react-three/fiber';
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <Canvas
+        shadows
+        camera={{
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [3, 2, 6],
+        }}
+    >
+        <App />
+    </Canvas>
+);
 
