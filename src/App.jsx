a7e2652018406488fe1adf6783f830925f27eba8
dosepehr <dosepehr@gmail.com>
2023-12-09 16:08:15 +0330

    add initial scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    return (
+        <>
+            <Canvas
+                camera={{
+                    fov: 45,
+                    near: 0.1,
+                    far: 200,
+                    position: [3, 2, 6],
+                }}
+            >
+                <OrbitControls />
+                <mesh position={[3, 0, 0]}>
+                    <sphereGeometry args={[1]} />
+                    <meshBasicMaterial color='yellow' />
+                </mesh>
+                <mesh position={[0, -1, 0]} rotation-x={-Math.PI / 2}>
+                    <planeGeometry args={[15, 15]} />
+                    <meshBasicMaterial color='green' />
+                </mesh>
+                <mesh position={[-3, 0, 0]}>
+                    <boxGeometry args={[2, 2, 2]} />
+                    <meshBasicMaterial color='skyblue' />
+                </mesh>
+            </Canvas>
+        </>
+    );
+};
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+export default App;
 
-export default App
