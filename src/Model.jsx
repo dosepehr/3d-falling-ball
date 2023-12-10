7c0dd70e33e742c33a909574d454f5c675a07793
dosepehr <dosepehr@gmail.com>
2023-12-10 19:07:30 +0330

    add bakedTexture for static meshes

--- src/Model.jsx
+++ src/Model.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { useGLTF } from '@react-three/drei';
+import { useGLTF, useTexture } from '@react-three/drei';
 const Model = () => {
     const { nodes } = useGLTF('/model/ball.glb');
+    const bakedTexture = useTexture('/model/ball.jpg');
+    bakedTexture.flipY = false;
     return (
         <>
             <group>
@@ -20,7 +22,9 @@ const Model = () => {
                     material={nodes.Plane_1.material}
                     position={[0, 17.501, 0]}
                     rotation={[Math.PI / 2, 0, 0]}
-                />
+                >
+                    <meshBasicMaterial map={bakedTexture} />
+                </mesh>
                 <mesh
                     castShadow
                     receiveShadow
@@ -28,7 +32,9 @@ const Model = () => {
                     material={nodes.Castle.material}
                     position={[0, 17.501, -0.001]}
                     rotation={[Math.PI / 2, 0, 0]}
-                />
+                >
+                    <meshBasicMaterial map={bakedTexture} />
+                </mesh>
                 <mesh
                     castShadow
                     receiveShadow
@@ -36,7 +42,9 @@ const Model = () => {
                     material={nodes.Plane_2.material}
                     position={[-0.006, 11.486, 5.682]}
                     rotation={[Math.PI / 2, 0, 0]}
-                />
+                >
+                    <meshBasicMaterial map={bakedTexture} />
+                </mesh>
             </group>
         </>
     );
