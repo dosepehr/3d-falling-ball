db716c9999b20fa1141e9d701cb1d8b438b5b389
dosepehr <dosepehr@gmail.com>
2023-12-10 19:01:53 +0330

    change camera properties

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,12 @@
-import { OrbitControls } from '@react-three/drei';
-import { useFrame } from '@react-three/fiber';
-import React, { useRef } from 'react';
+import { Center, OrbitControls } from '@react-three/drei';
 import Model from './Model';
-
 const App = () => {
-    const cube = useRef();
-    const group = useRef();
-    useFrame((state, delta) => {
-        const time = state.clock.elapsedTime;
-
-        const x = Math.cos(time) * 2;
-        const z = Math.sin(time) * 2;
-        group.current.position.x = x;
-        group.current.position.z = z;
-    });
     return (
         <>
-            <directionalLight position={[1, 2, 3]} intensity={1.5} castShadow />
-            <ambientLight intensity={1.5} />
             <OrbitControls />
-            <group ref={group}>
-                <mesh position={[3, 0, 0]} castShadow>
-                    <sphereGeometry args={[1]} />
-                    <meshStandardMaterial color='yellow' />
-                </mesh>
-                <mesh position={[-3, 0, 0]} castShadow ref={cube}>
-                    <boxGeometry args={[2, 2, 2]} />
-                    <meshStandardMaterial color='skyblue' />
-                </mesh>
-            </group>
-            <mesh position={[0, -1, 0]} rotation-x={-Math.PI / 2} receiveShadow>
-                <planeGeometry args={[15, 15]} />
-                <meshStandardMaterial color='green' />
-            </mesh>
-            <Model />
+            <Center>
+                <Model />
+            </Center>
         </>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
@@ -6,11 +5,12 @@ import { Canvas } from '@react-three/fiber';
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Canvas
         shadows
+        flat
         camera={{
             fov: 45,
             near: 0.1,
             far: 200,
-            position: [3, 2, 6],
+            position: [35, 30, 35],
         }}
     >
         <App />
